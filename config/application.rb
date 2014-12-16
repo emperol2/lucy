28e80a77a865b0e9c68af6ee609233b444e28e87
Nuttapon <emperol2@gmil.com>
2014-12-16 21:01:33 +0700

    Fix navbar collapse
    remove the bootstrap.min.js because it was duplication

--- config/application.rb
+++ config/application.rb
@@ -6,9 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-#config.assets.precompile =+ ['style.css']
-#config.serve_static_assets = true
-
 module Lucy
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
