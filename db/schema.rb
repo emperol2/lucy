49788ea7aaddb6fb343ca9a76752da66a1c7d38e
Nuttapon <emperol2@gmil.com>
2015-01-03 16:43:34 +0700

    created header and footer part
    - header
    - footer
    - separate pages
    - fixed css and js in different views and controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141210100029) do
+ActiveRecord::Schema.define(version: 20150103090854) do
+
+  create_table "about_us", force: true do |t|
+    t.string   "aboutus_text"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "homes", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "todos", force: true do |t|
     t.string   "title"
